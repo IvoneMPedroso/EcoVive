349db9897a59b87c2a44b0dee61285dfb4442967
Ivone Maria Pedroso <ivonemariapedroso@hotmail.com>
2024-09-20 20:44:45 -0300

    Atualização 20/09
    Atualização menu Hambúrguer

--- src/js/btnScroll.js
+++ src/js/btnScroll.js
@@ -7,3 +7,14 @@ window.addEventListener('scroll', function() {
     botaoFixo.style.opacity = '0'
   }
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('hamburger');
+  const menu = document.getElementById('menu');
+
+  // Add click event listener to the hamburger
+  hamburger.addEventListener('click', function () {
+      // Toggle the 'active' class to show/hide the menu
+      menu.classList.toggle('active');
+  });
+});
